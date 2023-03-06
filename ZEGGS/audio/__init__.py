a8e40f35e838fb739343ec44d34192f33be65bfe
Lasse Mejlvang Tvedt <himself@lassemt.com>
2023-03-06 16:31:02 +0100

    Fixed error messages

--- ZEGGS/audio/__init__.py
+++ ZEGGS/audio/__init__.py
@@ -26,12 +26,12 @@ elif platform.system() == "Darwin":
     try:
         output = subprocess.check_output(['sox', '--version'])
     except Exception as e:
-       raise Exception("Error calling sox dependency: " + str(e) + "\n\nHave you installed sox with 'apt-get install sox'?\n")
+       raise Exception("Error calling sox dependency: " + str(e) + "\n\nHave you installed sox with 'brew install sox'?\n")
 
     try:
         output = subprocess.check_output(['ffmpeg', '-version'])
     except Exception as e:
-       raise Exception("Error calling ffmpeg dependency: " + str(e) + "\n\nHave you installed ffmpeg with 'apt-get install ffmpeg'?\n")
+       raise Exception("Error calling ffmpeg dependency: " + str(e) + "\n\nHave you installed ffmpeg with 'brew install ffmpeg'?\n")
 
 
 else:
