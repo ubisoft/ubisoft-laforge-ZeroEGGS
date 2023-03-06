963c8f7183acd709b2a7dc0b2b83b8b0e39c2ddf
Lasse Mejlvang Tvedt <himself@lassemt.com>
2023-03-06 14:51:42 +0100

    Allow for darwin (Macos)

--- ZEGGS/audio/__init__.py
+++ ZEGGS/audio/__init__.py
@@ -20,5 +20,19 @@ elif platform.system() == "Linux":
     except Exception as e:
        raise Exception("Error calling ffmpeg dependency: " + str(e) + "\n\nHave you installed ffmpeg with 'apt-get install ffmpeg'?\n")
 
+elif platform.system() == "Darwin":
+    # Skipping because not sure if used at all
+    # os.environ['PATH'] += ";" + os.path.join(os.path.dirname(__file__), 'bin', 'psola', 'linux')
+    try:
+        output = subprocess.check_output(['sox', '--version'])
+    except Exception as e:
+       raise Exception("Error calling sox dependency: " + str(e) + "\n\nHave you installed sox with 'apt-get install sox'?\n")
+
+    try:
+        output = subprocess.check_output(['ffmpeg', '-version'])
+    except Exception as e:
+       raise Exception("Error calling ffmpeg dependency: " + str(e) + "\n\nHave you installed ffmpeg with 'apt-get install ffmpeg'?\n")
+
+
 else:
     raise Exception("Unsupported platform: {}".format(platform.system()))
